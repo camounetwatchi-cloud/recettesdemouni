bd4a8c89096b39c15163e6df3c031bdaa58744e9
camounetwatchi-cloud <camounetwatchi@gmail.com>
2025-11-12 13:44:20 +0100

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,13 +1,7 @@
 import React from 'react'
-   import ReactDOM from 'react-dom/client'
-   import App from './App.jsx'
-   import './index.css'
-
-   ReactDOM.createRoot(document.getElementById('root')).render(
-     <React.StrictMode>
-       <App />
-     </React.StrictMode>,
-   )
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
 // Mock de l'API storage pour le développement local
 if (!window.storage) {
@@ -30,3 +24,9 @@ if (!window.storage) {
     }
   };
 }
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
